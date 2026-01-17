f81451f6c7a2b74ba646b5e52683f6c49869b026
YooDxE3 <andreox27@gmail.com>
2026-01-17 18:35:36 -0300

    colocando nova forma de login e auth

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="viargr-v20.5.5"; // Versão atualizada para forçar atualização no celular
+const CACHE_NAME="viargr-v20.7.0"; // Versão atualizada para forçar atualização no celular
 const ASSETS=[
   "./",
   "./index.html",
