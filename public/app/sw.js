54d63f3e584709c023f48d995d72c54cd21bc9d0
YooDxE3 <andreox27@gmail.com>
2026-01-19 14:57:03 -0300

    adicionando compo (outro) e caixa para imput de função

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="viargr-v20.10.11"; // Versão atualizada para forçar atualização no celular
+const CACHE_NAME="viargr-v20.10.12"; // Versão atualizada para forçar atualização no celular
 const ASSETS=[
   "./",
   "./index.html",
