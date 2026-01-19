8b4a9dc6b0f0c9c5c2e19f60dfb7fcf30aa03ca2
YooDxE3 <andreox27@gmail.com>
2026-01-19 20:15:03 -0300

    correção da tela de rede e fornecedores

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="viargr-v20.12.15"; // Versão atualizada para forçar atualização no celular
+const CACHE_NAME="viargr-v20.12.16"; // Versão atualizada para forçar atualização no celular
 const ASSETS=[
   "./",
   "./index.html",
