435b06b095852cc048bcb716785e2d8f408c9e13
YooDxE3 <andreox27@gmail.com>
2026-01-20 16:24:48 -0300

    arrumando logica de login

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="viargr-v20.13.4"; // Versão atualizada para forçar atualização no celular
+const CACHE_NAME="viargr-v20.13.5"; // Versão atualizada para forçar atualização no celular
 const ASSETS=[
   "./",
   "./index.html",
