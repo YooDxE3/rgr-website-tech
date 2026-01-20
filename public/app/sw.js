8f29e170897d9ceed495112846d8524977dbc610
YooDxE3 <andreox27@gmail.com>
2026-01-20 15:44:35 -0300

    trazendo bindUnits e loadDB novamente

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="viargr-v20.13.0"; // Versão atualizada para forçar atualização no celular
+const CACHE_NAME="viargr-v20.13.1"; // Versão atualizada para forçar atualização no celular
 const ASSETS=[
   "./",
   "./index.html",
