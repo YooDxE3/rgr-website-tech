2e8923787cb95535a832fa0006a194c1ffbfd657
YooDxE3 <andreox27@gmail.com>
2026-01-23 18:55:36 -0300

    atualizando motor

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="viargr-v20.15.16"; // Versão atualizada para forçar atualização no celular
+const CACHE_NAME="viargr-v20.15.17"; // Versão atualizada para forçar atualização no celular
 const ASSETS=[
   "./",
   "./index.html",
