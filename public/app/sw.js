5750376456e5f1f7c417f23dc37f30f68767ebf8
YooDxE3 <andreox27@gmail.com>
2026-01-24 18:27:22 -0300

    arrumando telas do gestor

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="viargr-v20.16.9"; // Versão atualizada para forçar atualização no celular
+const CACHE_NAME="viargr-v20.16.10"; // Versão atualizada para forçar atualização no celular
 const ASSETS=[
   "./",
   "./index.html",
