4bb822e0bf1ccef0294674dd78a19da5fca04342
YooDxE3 <andreox27@gmail.com>
2026-01-24 18:36:13 -0300

    criando modal para aprovar dastros novos

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="viargr-v20.16.10"; // Versão atualizada para forçar atualização no celular
+const CACHE_NAME="viargr-v20.16.11"; // Versão atualizada para forçar atualização no celular
 const ASSETS=[
   "./",
   "./index.html",
