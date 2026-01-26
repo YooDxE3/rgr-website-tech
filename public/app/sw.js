09a22fcac4499e91a55e912bc03a1447b1d32cac
YooDxE3 <andreox27@gmail.com>
2026-01-26 19:50:15 -0300

    fazendo mudanças na tela gestor (adicionando funções)

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="viargr-v20.16.21"; // Versão atualizada para forçar atualização no celular
+const CACHE_NAME="viargr-v20.16.22"; // Versão atualizada para forçar atualização no celular
 const ASSETS=[
   "./",
   "./index.html",
